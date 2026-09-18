876412f6528ecb77472cb549968aba4c54f23d0e
yunfei00 <goodman_yunfei@163.com>
2026-09-18 17:52:35 +0800

    feat: enable UE reports for final reference metrics

--- devices/cmw500_controller.py
+++ devices/cmw500_controller.py
@@ -254,6 +254,7 @@ class RealCMW500(InstrumentBase):
         self.current_cable_loss = loss
 
         fixed_commands = (
+            ("CONFigure:LTE:SIGN:UEReport:ENABle ON", "lte_prepare_run.ue_report"),
             (
                 "CONFigure:LTE:SIGN:UL:PUSCh:TPC:SET MAXP",
                 "lte_prepare_run.ul_tpc_maxp",
